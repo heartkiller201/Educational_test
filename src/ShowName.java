76a52999c6c2a90d4f1fe3e744b3d30d2619eeb9
Vladislav Tretyak <tretyak.vlad201@gmail.com>
2022-06-23 21:30:22 +0300

    Added .iml file

--- src/ShowName.java
+++ src/ShowName.java
@@ -2,7 +2,7 @@ public class ShowName {
 
     public static void main(String[] args) {
         System.out.println("Привет народ");
-        System.out.println("И пока, пока");
+        System.out.println("И пока, пока, bye");
 
 
     }
