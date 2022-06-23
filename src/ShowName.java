de5d012ca114f159fceef97790c92e4c21bdebd5
Vladislav Tretyak <tretyak.vlad201@gmail.com>
2022-06-24 00:16:48 +0300

    added one more println

--- src/ShowName.java
+++ src/ShowName.java
@@ -3,6 +3,7 @@ public class ShowName {
     public static void main(String[] args) {
         System.out.println("Привет народ");
         System.out.println("И пока");
+        System.out.println("For master only");
 
 
     }
